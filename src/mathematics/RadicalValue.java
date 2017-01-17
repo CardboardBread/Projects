6fb2228cc515d770fa14a15043fc33864ed457d5
Michael Brown <mike_brown@rogers.com>
2017-01-16 21:08:24 -0500

    k

--- src/mathematics/RadicalValue.java
+++ src/mathematics/RadicalValue.java
@@ -6,40 +6,39 @@ public class RadicalValue {
 
 	public static void main(String[] args) {
 
-		/*
-		 * int radicand = 2; String number = "1."; String[] guesses = new
-		 * String[10]; for (int i = 0; i < 10; i++) { String guess = number + i;
-		 * guesses[i] = number + i; System.out.println(guess); }
-		 * System.out.println();
-		 * 
-		 * String[] squares = new String[10]; for (int i = 0; i < 10; i++) {
-		 * Double numGuess = Double.parseDouble(guesses[i]); squares[i] =
-		 * Double.toString(Math.pow(Double.parseDouble(guesses[i]), 2));
-		 * System.out.println(squares[i]); } System.out.println();
-		 * 
-		 * Double closest = 100d; for (int i = 0; i < 10; i++) { double distance
-		 * = Math.abs(radicand - Double.parseDouble(squares[i]));
-		 * System.out.println(distance); if (distance < closest) { closest =
-		 * distance; } } System.out.println(closest);
-		 */
+		String square = "2";
+		int iSquare = 2;
+		String radicand = "1.";
+		String solution = radicand;
 
-		int radicand = 2;
-		double radical = Math.sqrt(radicand);
-		double floor = Math.floor(radical);
-		String guess = Double.toString(floor).substring(0, Double.toString(floor).length() - 1);
-		System.out.println("Original: " + guess);
-		for (int depth = 1; depth <= 5; depth++) { // test down to 10 decimal points
-			
-			int closest;
-			for (int n = 1; n < 10; n++) {
-				System.out.println(guess + n);
-				String strDecimal = guess + n;
-				double decimal = Double.parseDouble(strDecimal);
-				double distance = radicand - decimal;
-				
+		for (int k = 0; k < 40; k++) {
+			String[] radicands = new String[10];
+			for (int i = 0; i < 10; i++) {
+				radicands[i] = solution + i;
 			}
-			guess += "3";
-		}
-	}
 
+			double[] squares = new double[10];
+			for (int i = 0; i < 10; i++) {
+				//System.out.print(radicands[i] + " ");
+				double test = Double.parseDouble(radicands[i]);
+				squares[i] = Math.pow(test, 2);
+			}
+			//System.out.println();
+
+			double[] deltas = new double[10];
+			double lowest = 100d;
+			int lowPos = 0;
+			for (int i = 0; i < 10; i++) {
+				deltas[i] = Math.abs(squares[i] - iSquare);
+				if (deltas[i] < lowest) {
+					lowest = deltas[i];
+					lowPos = i;
+				}
+				//System.out.print(deltas[i] + " ");
+			}
+			String out = radicands[lowPos];
+			solution += out.substring(out.length() - 1);
+		}
+		System.out.println(solution);
+	}
 }
