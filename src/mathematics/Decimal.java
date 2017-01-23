6d0ec2aff9e8e01f99d3eb764e4dfc0a873bab51
Michael Brown <mike_brown@rogers.com>
2017-01-23 08:28:46 -0500

    yes

---
+++ src/mathematics/Decimal.java
@@ -0,0 +1,53 @@
+package mathematics;
+
+public class Decimal {
+
+	private String value;
+	private int decimalIndex;
+	private boolean decimal;
+
+	public <E extends Number> Decimal(E value) {
+		this.value = value.toString();
+	}
+	
+	public Decimal(Decimal in) {
+		this.value = in.value;
+		this.decimalIndex = in.decimalIndex;
+		this.decimal = in.decimal;
+	}
+
+	private int decimalIndex() {
+
+		return decimalIndex = value.indexOf('.');
+	}
+
+	public String getRaw() {
+		return value;
+	}
+
+	private long value() {
+		String whole = "";
+
+		if (decimalIndex < 0) {
+			whole = value;
+		} else {
+			whole = value.substring(0, decimalIndex);
+			whole += value.substring(decimalIndex + 1, value.length());
+		}
+		
+		try {
+			return Long.parseLong(whole);
+		} catch (NumberFormatException nfe) {
+			nfe.printStackTrace();
+		}
+		return 0L;
+	}
+
+	public static void main(String[] args) {
+		Decimal d = new Decimal(Double.MAX_VALUE);
+		System.out.println(d.getRaw());
+		System.out.println(d.decimalIndex());
+		System.out.println(d.value());
+	}
+
+}
