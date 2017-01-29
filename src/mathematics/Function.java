c93a7f8bce0cab931f0982038387c26f314039a2
Michael Brown <mike_brown@rogers.com>
2017-01-28 23:40:48 -0500

    math fix?

--- src/mathematics/Function.java
+++ src/mathematics/Function.java
@@ -2,22 +2,15 @@ package mathematics;
 
 import java.math.BigDecimal;
 
-public class Function {
+public abstract class Function extends Argument {
 
-	private char identifier;
-	private Variable input;
-	private Argument outputStretch;
-	private Argument outputShift;
-	private Argument inputStretch;
-	private Argument inputShift;
+	protected char identifier;
+	protected Variable input;
+	protected Argument composed;
 
-	public Function(char identifier, Argument outputStretch, Argument outputShift, Argument inputStretch,
-			Argument inputShift, Variable input) {
+	public Function(char identifier, Variable input) {
+		super("" + identifier, false);
 		this.identifier = identifier;
-		this.inputShift = inputShift;
-		this.inputStretch = inputStretch;
-		this.outputShift = outputShift;
-		this.outputStretch = outputStretch;
 		this.input = input;
 	}
 
@@ -25,19 +18,12 @@ public class Function {
 		return identifier;
 	}
 
-	public Argument inputArgument() {
-		return new Parentheses(new Product(inputStretch, new Parentheses(new Difference(input, inputShift))));
+	public Argument composed() {
+		return composed;
 	}
 
-	public Argument asArgument() {
-		return new Sum(new Product(outputStretch, inputArgument()), outputShift);
-	}
-
-	public BigDecimal evaluate(BigDecimal value) {
-		Argument in = new Parentheses(
-				new Product(inputStretch, new Parentheses(new Difference(new Constant(value), inputShift))));
-		Argument out = new Sum(new Product(outputStretch, in), outputShift);
-		return out.eval();
+	public BigDecimal evaluate(BigDecimal input) {
+		return composed.evaluate();
 	}
 
 }
