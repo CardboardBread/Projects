3cd9668a8d9bc53c05ae25fa3ec045b1e2d55741
Michael Brown <michaelmbcubed@gmail.com>
2018-10-17 08:46:15 -0400

    Ground-Up rework of Decimal
    making it more of a accessible class that can be used in other programs,
    rather than a proof of concept in a single file

--- src/Decimal.java
+++ src/Decimal.java
@@ -1,55 +1,119 @@
-import java.util.Random;
-
 public class Decimal {
-
-	private String value;
-	private int decimalIndex;
-	private boolean decimal;
-
-	public <E extends Number> Decimal(E value) {
-		this.value = value.toString();
-	}
 	
-	public Decimal(Decimal in) {
-		this.value = in.value;
-		this.decimalIndex = in.decimalIndex;
-		this.decimal = in.decimal;
-	}
-
-	private int decimalIndex() {
-
-		return decimalIndex = value.indexOf('.');
-	}
-
-	public String getRaw() {
-		return value;
-	}
-
-	private long value() {
-		String whole = "";
-
-		if (decimalIndex < 0) {
-			whole = value;
-		} else {
-			whole = value.substring(0, decimalIndex);
-			whole += value.substring(decimalIndex + 1, value.length());
+	public static final char decimalCharacter = '.';
+	
+	public static void relative(Decimal first, Decimal second) {
+		if (first.getWholeLength() > second.getWholeLength()) {
+			int lengthDiff = first.getWholeLength() - second.getWholeLength();
+			
+			for (int i = 0; i < lengthDiff; i++) {
+				second.addLeadingZero();
+			}
+		} else if (first.getWholeLength() < second.getWholeLength()) {
+			int lengthDiff = second.getWholeLength() - first.getWholeLength();
+			
+			for (int i = 0; i < lengthDiff; i++) {
+				first.addLeadingZero();
+			}
 		}
 		
-		try {
-			return Long.parseLong(whole);
-		} catch (NumberFormatException nfe) {
-			nfe.printStackTrace();
+		if (first.getFractionLength() > second.getFractionLength()) {
+			int lengthDiff = first.getFractionLength() - second.getFractionLength();
+			
+			for (int i = 0; i < lengthDiff; i++) {
+				second.addTrailingZero();
+			}
+		} else if (first.getFractionLength() < second.getFractionLength()) {
+			int lengthDiff = second.getFractionLength() - first.getFractionLength();
+			
+			for (int i = 0; i < lengthDiff; i++) {
+				first.addTrailingZero();
+			}
 		}
-		return 0L;
+	}
+	
+	public static Decimal sum(Decimal first, Decimal second) {
+		Decimal.relative(first, second);
+		
+		String firstRaw = first.getRaw();
+		String secondRaw = second.getRaw();
+		int length = first.getRaw().length();
+		
+		int carry = 0;
+		for (int i = length - 1; i > -1; i--) {
+			System.out.println(i);
+			carry = add(firstRaw.charAt(i), secondRaw.charAt(i));
+		}
+		
+		return null;
+	}
+	
+	private static char add(char first, char second) {
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Decimal dec = new Decimal("1234", 1);
+		Decimal dek = new Decimal("1234", 3);
+		System.out.println(dec.getFormat());
+		System.out.println(dec.getWholeLength());
+		System.out.println(dec.getFractionLength());
+		System.out.println();
+		System.out.println(dek.getFormat());
+		System.out.println(dek.getWholeLength());
+		System.out.println(dek.getFractionLength());
+		Decimal.relative(dec, dek);
+		System.out.println(dec.getFormat());
+		System.out.println(dek.getFormat());
+		System.out.println(dec.getRaw());
+		System.out.println(dek.getRaw());
+		Decimal.sum(dec, dek);
 	}
 
-	public static void main(String[] args) {
-		Random rand = new Random();
-		Decimal d = new Decimal(rand.nextFloat());
-		System.out.println(d.getRaw());
-		System.out.println(d.getRaw().charAt(1));
-		System.out.println(d.decimalIndex());
-		System.out.println(d.value());
+	private String rawNumber;
+	private int decimalIndex;
+	
+	public Decimal(String rawNumber) {
+		// scan for a decimal point, if there is none, place one at the end
+	}
+	
+	public Decimal(String rawNumber, int decimalIndex) {
+		this.rawNumber = rawNumber;
+		this.decimalIndex = decimalIndex;
+		while (this.decimalIndex > this.rawNumber.length() - 1) {
+			addTrailingZero();
+		}
+	}
+	
+	public int getWholeLength() {
+		return 0 + decimalIndex;
+	}
+	
+	public int getFractionLength() {
+		return rawNumber.length() - decimalIndex;
+	}
+	
+	public String getRaw() {
+		return rawNumber;
+	}
+	
+	public void addLeadingZero() {
+		rawNumber = "0" + rawNumber;
+		decimalIndex += 1;
+	}
+	
+	public void addTrailingZero() {
+		rawNumber = rawNumber + "0";
+	}
+	
+	public String getFormat() {
+		String frontHalf = rawNumber.substring(0, decimalIndex);
+		String backHalf = rawNumber.substring(decimalIndex, rawNumber.length());
+		return frontHalf + decimalCharacter + backHalf;
+	}
+	
+	public void cleanZeroes() {
+		
 	}
 
 }
